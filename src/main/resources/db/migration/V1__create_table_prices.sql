cf7fa85481b24b57b1f7511404a48e919b795340
Cristian <castagna.cristian16@gmail.com>
2023-02-04 22:42:57 -0300

    flyway configuration

---
+++ src/main/resources/db/migration/V1__create_table_prices.sql
@@ -0,0 +1,12 @@
+-- Create the PRICES table
+CREATE TABLE PRICES
+(
+    BRAND_ID   INT            NOT NULL,
+    START_DATE TIMESTAMP      NOT NULL,
+    END_DATE   TIMESTAMP      NOT NULL,
+    PRICE_LIST INT            NOT NULL,
+    PRODUCT_ID INT            NOT NULL,
+    PRIORITY   INT            NOT NULL,
+    PRICE      DECIMAL(10, 2) NOT NULL,
+    CURR       VARCHAR(3)     NOT NULL
+);
