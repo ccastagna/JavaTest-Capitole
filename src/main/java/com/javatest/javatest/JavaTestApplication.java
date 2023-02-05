5407c6994e0df67271fa40d8be8e44f7f8a8201e
Cristian <castagna.cristian16@gmail.com>
2023-02-05 16:39:21 -0300

    Configure swagger with open-api

--- src/main/java/com/javatest/javatest/JavaTestApplication.java
+++ src/main/java/com/javatest/javatest/JavaTestApplication.java
@@ -1,12 +1,12 @@
 package com.javatest.javatest;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@EnableJpaRepositories
+@OpenAPIDefinition
 public class JavaTestApplication {
 
 	public static void main(String[] args) {
