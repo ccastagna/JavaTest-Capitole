d7a24a67f079b963fee4465a2a6cac6e6414999f
Cristian <castagna.cristian16@gmail.com>
2023-02-05 16:12:22 -0300

    Refactor Controller

--- src/main/java/com/javatest/javatest/infrastructure/drivers/controllers/ProductPriceController.java
+++ src/main/java/com/javatest/javatest/infrastructure/drivers/controllers/ProductPriceController.java
@@ -4,10 +4,11 @@ import com.javatest.javatest.application.exceptions.ProductPriceNotFoundExceptio
 import com.javatest.javatest.application.usecases.getproductprice.GetProductPriceRequest;
 import com.javatest.javatest.application.usecases.getproductprice.GetProductPriceResponse;
 import com.javatest.javatest.infrastructure.drivers.intetrfaces.IGetProductPriceUseCase;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.format.DateTimeParseException;
@@ -22,14 +23,15 @@ public class ProductPriceController {
     }
 
     @GetMapping("/v1/brands/{brandId}/products/{productId}")
-    public ResponseEntity<GetProductPriceResponse> getProductPrice(
+    @ResponseStatus(HttpStatus.OK)
+    public GetProductPriceResponse getProductPrice(
             @PathVariable Integer brandId,
             @PathVariable Integer productId,
             @RequestParam(name = "date") String date) throws ProductPriceNotFoundException, DateTimeParseException {
 
         GetProductPriceRequest getProductPriceRequest = new GetProductPriceRequest(brandId, productId, date);
 
-        return ResponseEntity.ok().body(this.getProductPriceUseCase.execute(getProductPriceRequest));
+        return this.getProductPriceUseCase.execute(getProductPriceRequest);
     }
 
 }
